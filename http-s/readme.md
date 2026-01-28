b4a9b950956920a075057a73bf6bea662543184e
Alex Pascal <futupas@gmail.com>
2026-01-28 22:58:39 +0100

    add: https, setup ready (but the chat is very bad)

--- http-s/readme.md
+++ http-s/readme.md
@@ -1,10 +1,4 @@
-Ports: 6080 for noVNC, 8080 for chat
+Ports: 6080 for noVNC, 8080 and 8443 for chat
 
-Have to run `service apache2 start` after booting
-
-WireShark filter: `tcp.port == 8080 && http`
-
-## ToDo
-
-* https apache (8443)
-
+WireShark filter HTTP: `tcp.port == 8080 && ip.dst == 172.20.0.2 && http`
+WireShark filter HTTPS: `tcp.port == 8443 && ip.dst == 172.20.0.2`
--- http-s/start.sh
+++ http-s/start.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
-# set -e
+set -e
 
-# a2enmod ssl
+# --- 1. Start Apache config for HTTP + HTTPS ---
+# Enable SSL module
+a2enmod ssl
 
-# mkdir -p /etc/apache2/ssl
+# Create SSL folder
+mkdir -p /etc/apache2/ssl
 
-# openssl req -x509 -nodes -days 365 \
-#   -newkey rsa:2048 \
-#   -keyout /etc/apache2/ssl/apache.key \
-#   -out /etc/apache2/ssl/apache.crt \
-#   -subj "/C=CZ/ST=Brno/L=Brno/O=AlexPascal/OU=Dev/CN=localhost"
+# Generate self-signed certificate (Alex Pascal, Brno)
+openssl req -x509 -nodes -days 365 \
+  -newkey rsa:2048 \
+  -keyout /etc/apache2/ssl/apache.key \
+  -out /etc/apache2/ssl/apache.crt \
+  -subj "/C=CZ/ST=Brno/L=Brno/O=AlexPascal/OU=Dev/CN=localhost"
 
-# Start Apache
-service apache2 start
+# # --- 2. Update ports.conf to listen on 8080 (HTTP) and 8443 (HTTPS) ---
+# sed -i '/Listen 80/c\Listen 8080' /etc/apache2/ports.conf
+# grep -q "Listen 8443" /etc/apache2/ports.conf || echo "Listen 8443" >> /etc/apache2/ports.conf
 
-# Launch desktop + VNC
+# # --- 3. Create or modify SSL VirtualHost ---
+# SSL_CONF="/etc/apache2/sites-available/default-ssl.conf"
+
+# cat > $SSL_CONF <<EOL
+# <VirtualHost *:8443>
+#     ServerAdmin webmaster@localhost
+#     DocumentRoot /var/www/html
+
+#     SSLEngine on
+#     SSLCertificateFile      /etc/apache2/ssl/apache.crt
+#     SSLCertificateKeyFile   /etc/apache2/ssl/apache.key
+
+#     <Directory /var/www/html>
+#         Options Indexes FollowSymLinks
+#         AllowOverride All
+#         Require all granted
+#     </Directory>
+# </VirtualHost>
+# EOL
+
+# # Enable SSL site
+# a2ensite default-ssl
+
+# --- 4. Start Apache in foreground ---
+apache2ctl -D FOREGROUND & # does not actually works
+
+# --- 5. Start LXDE desktop with VNC ---
 /startup.sh
